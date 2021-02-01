da95a5cdecf5b38795eba65dd036da8893e84c60
MoneyGen <78038943+MoneyGen@users.noreply.github.com>
2021-02-01 19:21:57 +0100

    Add files via upload

---
+++ src/js/main.js
@@ -0,0 +1,36 @@
+//Envoie le mail via ajax
+
+('#sending_form').click(function(e){
+    e.preventDefault();
+    var data = {
+        email: $('#email').val(),
+        name: $('#name').val(),
+        objet: $('#firstname').val(),
+        message: $('#message').val()
+    };
+    $.ajax({
+        url: "mail.php",
+        type: 'POST',
+        data: data,
+        success: function(data) {
+            $('#js_alert_success').css({'display' : 'block'});
+            setTimeout(function(){
+                $('#js_alert_success').css({'display' : 'none'});
+                $('#email').val("");
+                $('#name').val("");
+                $('#firstname').val("");
+                $('#message').val("")
+            }, 3000);
+        },
+        error: function(data) {
+            $('#js_alert_danger').css({'display' : 'block'});
+            setTimeout(function(){
+                $('#js_alert_danger').css({'display' : 'none'});
+                $('#email').val("");
+                $('#name').val("");
+                $('#firstname').val("");
+                $('#message').val("")
+            }, 3000);
+        }
+    });
+});
